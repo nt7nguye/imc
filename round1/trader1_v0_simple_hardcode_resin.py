d3b5ecf8417bd50ff1f380a5b4dc7106080c4688
Tan Nguyen <45516852+nt7nguye@users.noreply.github.com>
2025-04-09 01:13:49 -0400

    chore: using boilerplate

---
+++ round1/trader1_v0_simple_hardcode_resin.py
@@ -0,0 +1,197 @@
+import json
+from typing import Any, List
+import jsonpickle
+
+from datamodel import (
+    Listing,
+    Observation,
+    Order,
+    OrderDepth,
+    ProsperityEncoder,
+    Symbol,
+    Trade,
+    TradingState,
+)
+
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+        self.max_log_length = 3750
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(
+        self,
+        state: TradingState,
+        orders: dict[Symbol, list[Order]],
+        conversions: int,
+        trader_data: str,
+    ) -> None:
+        base_length = len(
+            self.to_json(
+                [
+                    self.compress_state(state, ""),
+                    self.compress_orders(orders),
+                    conversions,
+                    "",
+                    "",
+                ]
+            )
+        )
+
+        # We truncate state.traderData, trader_data, and self.logs to the same max. length to fit the log limit
+        max_item_length = (self.max_log_length - base_length) // 3
+
+        print(
+            self.to_json(
+                [
+                    self.compress_state(
+                        state, self.truncate(state.traderData, max_item_length)
+                    ),
+                    self.compress_orders(orders),
+                    conversions,
+                    self.truncate(trader_data, max_item_length),
+                    self.truncate(self.logs, max_item_length),
+                ]
+            )
+        )
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState, trader_data: str) -> list[Any]:
+        return [
+            state.timestamp,
+            trader_data,
+            self.compress_listings(state.listings),
+            self.compress_order_depths(state.order_depths),
+            self.compress_trades(state.own_trades),
+            self.compress_trades(state.market_trades),
+            state.position,
+            self.compress_observations(state.observations),
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing.symbol, listing.product, listing.denomination])
+
+        return compressed
+
+    def compress_order_depths(
+        self, order_depths: dict[Symbol, OrderDepth]
+    ) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append(
+                    [
+                        trade.symbol,
+                        trade.price,
+                        trade.quantity,
+                        trade.buyer,
+                        trade.seller,
+                        trade.timestamp,
+                    ]
+                )
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sugarPrice,
+                observation.sunlightIndex,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+    def to_json(self, value: Any) -> str:
+        return json.dumps(value, cls=ProsperityEncoder, separators=(",", ":"))
+
+    def truncate(self, value: str, max_length: int) -> str:
+        if len(value) <= max_length:
+            return value
+
+        return value[: max_length - 3] + "..."
+
+
+logger = Logger()
+
+
+class Product:
+    RESIN = "RAINFOREST_RESIN"
+    KELP = "KELP"
+
+
+class Trader:
+    LIMIT = {Product.RESIN: 50, Product.KELP: 50}
+
+    def trade_resin(self, state: TradingState) -> list[Order]:
+        position = state.position.get(Product.RESIN, 0)
+        orders = []
+
+        # Attemp to clear out position
+        if position > 0:
+            orders.append(Order(Product.RESIN, 10001, -position))
+        elif position < 0:
+            orders.append(Order(Product.RESIN, 9999, -position))
+
+        orders.extend(
+            [
+                Order(Product.RESIN, 10004, -5),
+                Order(Product.RESIN, 10003, -2),
+                Order(Product.RESIN, 10002, -1),
+                Order(Product.RESIN, 9998, 1),
+                Order(Product.RESIN, 9997, 2),
+                Order(Product.RESIN, 9996, 5),
+            ]
+        )
+        return orders
+
+    def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
+        traderObject = {}
+        if state.traderData != None and state.traderData != "":
+            traderObject = jsonpickle.decode(state.traderData)
+
+        result = {}
+        result[Product.RESIN] = self.trade_resin(state)
+
+        conversions = 1
+        traderData = jsonpickle.encode(traderObject)
+        logger.flush(state, result, conversions, traderData)
+
+        return result, conversions, traderData
+
+
+"""
+Profit summary:
+Round 1 day -2: 12,533
+Round 1 day -1: 12,704
+Round 1 day 0: 11,700
+Total profit: 36,937
+
+Live: ~700
+"""
--- trader.py
+++ trader.py
@@ -1,4 +1,4 @@
-from datamodel import OrderDepth, UserId, TradingState, Order
+from round1.datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 
 
